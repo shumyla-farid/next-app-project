72f33fba420b3e2752936a901985ec42d2b21bb8
Shumyla Farid <64152859+shumyla-farid@users.noreply.github.com>
2025-11-05 20:26:33 +0000

    loading

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,10 @@
-export default function BlogPage() {
+export default async function BlogPage() {
   const id = "123";
+
+  await new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(true);
+    }, 2000),
+  );
   return <h1>Blog Page id={id}</h1>;
 }
